2f7cff114a162c418eb543fe96a66be0be518b93
Your Actual Name <Your Actual Email>
2016-11-14 09:40:48 +0100

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
@@ -42,4 +44,5 @@ class ArticlesController < ApplicationController
 
     redirect_to article_path(@article)
   end
+
 end
